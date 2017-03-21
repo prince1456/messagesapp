9d80f304044ed916d9a9a7fcf7824fc923a4e63a
Ali Akbar Alizada <akbar.alizadeh5@gmail.com>
2017-03-21 15:51:10 -0700

    Add and test 14 units

---
+++ public/posts_controller_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+# Devise::Test::ControllerHelpers
+RSpec.describe PostsController, type: Devise::Test::ControllerHelpers do
+
+
+end
--- spec/features/users/user_index_spec.rb
+++ spec/features/users/user_index_spec.rb
@@ -12,10 +12,7 @@ feature 'User index page', :devise do
     Warden.test_reset!
   end
 
-  # Scenario: User listed on index page
-  #   Given I am signed in
-  #   When I visit the user index page
-  #   Then I see my own email address
+  # Scenario: User can see his email in the in the users_path
   scenario 'user sees own email address' do
     user = FactoryGirl.create(:user)
     login_as(user, scope: :user)
